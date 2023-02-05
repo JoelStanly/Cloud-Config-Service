a5fda7aee205e1e8e954a451a322b88c5680ed32
Joel Stanly A <38435145+JoelStanly@users.noreply.github.com>
2023-02-05 15:14:02 +0530

    CloudConfig

---
+++ src/main/java/com/aqms/cloud/CloudConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.aqms.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+// Configuration Server based on the Cloud configuration from Github Repository
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class CloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigServerApplication.class, args);
+	}
+
+}
